4c475f9aa86d5011f8413f772ef33367a7393af5
Nachocab22 <ignacio.caballeromarin@alum.uca.es>
2024-04-01 12:43:12 +0200

    Añadidos iconos

--- src/components/forms/FormDni.js
+++ src/components/forms/FormDni.js
@@ -27,16 +27,25 @@ const FormDni = () => {
     return (
         <div className="p-2 flex-col justify-start items-start gap-0.5">
             <label htmlFor="dni" className="content-stretch text-slate-700 text-base font-normal font-['IBM Plex Sans'] leading-normal tracking-tight">DNI</label>
-            <input 
-                id="dniInput" 
-                type="text" 
-                value={dni}
-                onChange={handleDniChange} 
-                className="w-full self-stretch h-12 p-3 bg-neutral-50 rounded justify-start items-start gap-2.5 flex-1" 
-                placeholder='DNI'
-                maxLength={9}
-            />
-            {!isValid && <span className="text-red-600 text-sm">El DNI no es válido</span>}
+            <div className="w-full">
+                <div className="relative w-full">
+                    <input 
+                        id="dniInput" 
+                        type="text" 
+                        value={dni}
+                        onChange={handleDniChange} 
+                        className="w-full self-stretch h-12 p-3 bg-neutral-50 rounded justify-start items-start flex-1" 
+                        placeholder='DNI'
+                        maxLength={9}
+                    />
+                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
+                        </svg>
+                    </div>
+                </div>
+                {!isValid && <span className="text-red-600 text-sm">El DNI no es válido</span>}
+            </div>
         </div>
     );
 };
--- src/components/forms/FormPhone.js
+++ src/components/forms/FormPhone.js
@@ -11,15 +11,22 @@ const FormPhone = () => {
     return (
         <div className="p-2 flex-col justify-start items-start gap-0.5">
             <label htmlFor="phone" className="content-stretch text-slate-700 text-base font-normal font-['IBM Plex Sans'] leading-normal tracking-tight">Teléfono</label>
-            <input 
-                id="phone" 
-                type="tel" 
-                value={phone}
-                onChange={handlePhoneChange}
-                className="w-full self-stretch h-12 p-3 bg-neutral-50 rounded justify-start items-start gap-2.5 flex-1" 
-                placeholder='Teléfono'
-                isRequired={false}
-            />
+            <div className="relative w-full">
+                <input 
+                    id="phone" 
+                    type="tel" 
+                    value={phone}
+                    onChange={handlePhoneChange}
+                    className="w-full self-stretch h-12 p-3 bg-neutral-50 rounded justify-start items-start gap-2.5 flex-1" 
+                    placeholder='Teléfono'
+                    isRequired={false}
+                />
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500 scale-x-[-1] -rotate-12">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                    </svg>
+                </div>
+            </div>
         </div>
     );
 };
