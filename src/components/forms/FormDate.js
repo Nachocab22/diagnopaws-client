f2f176483887fd66a6772af596f43f852176c649
Nachocab22 <ignacio.caballeromarin@alum.uca.es>
2024-04-04 11:15:46 +0200

    Arreglos pequeños de clase

--- src/components/forms/FormDate.js
+++ src/components/forms/FormDate.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FormDate = ({ label }) => {
     return (
-        <div className="p-2 flex-col justify-start items-start gap-0.5 inline-flex">
+        <div className="p-2 flex-col justify-start items-start gap-0.5">
             <label className="content-stretch text-slate-700 text-base font-normal font-['IBM Plex Sans'] leading-normal tracking-tight">{label}</label>
             <div className="relative w-full">
                 <input type="date" className="w-full h-12 p-3 bg-neutral-50 rounded flex-col justify-start items-start gap-2.5 flex" />
--- src/components/forms/FormText.js
+++ src/components/forms/FormText.js
@@ -6,7 +6,7 @@ const FormText = ({ label, placeholder, value, onChange = () => {}}) => {
         <div className="p-2 flex-col justify-start items-start gap-0.5">
             <label className="content-stretch text-slate-700 text-base font-normal font-['IBM Plex Sans'] leading-normal tracking-tight">{label}</label>
             <input type="text" 
-            className="w-full self-stretch h-12 p-3 bg-neutral-50 rounded justify-start items-start gap-2.5 flex-1" 
+            className="w-full self-stretch h-12 p-3 bg-neutral-50 rounded justify-start items-start gap-2.5" 
             placeholder={placeholder} 
             value={value}
             onChange={onChange}
