54d0a4502390711aa9c7dda80f4ba4a2c2ff949c
Nachocab22 <ignacio.caballeromarin@alum.uca.es>
2024-06-06 20:07:21 +0200

    Delete VerticalDivider.jsx

--- src/components/VerticalDivider.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const VerticalDivider = (color) => {
-    return (
-        <div className='h-auto bg-zinc-50 w-0.5 my-5'></div>
-    );
-};
-
-export default VerticalDivider;
