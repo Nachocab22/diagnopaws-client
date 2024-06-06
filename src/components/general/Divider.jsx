d12185d5c28375ed9222759110355ac1ea7a751f
Nachocab22 <ignacio.caballeromarin@alum.uca.es>
2024-06-06 20:07:30 +0200

    Create Divider.jsx

---
+++ src/components/general/Divider.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const VerticalDivider = (color) => {
+    return (
+        <div className='w-auto bg-zinc-50 h-0.5 rounded-full mb-3'></div>
+    );
+};
+
+export default VerticalDivider;
