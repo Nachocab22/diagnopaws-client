4c1d63f584ec6b29642ec6f9cedb562649ecf58c
Nachocab22 <ignacio.caballeromarin@alum.uca.es>
2024-06-07 13:06:53 +0200

    Update FormPhone.jsx

--- src/components/forms/FormPhone.jsx
+++ src/components/forms/FormPhone.jsx
@@ -1,12 +1,5 @@
 import { useState } from "react";
-const FormPhone = () => {
-
-    const [phone, setPhone] = useState("");
-
-    const handlePhoneChange = (e) => {
-        const {value} = e.target;
-        setPhone(value);
-    };
+const FormPhone = ({value, onChange}) => {
 
     return (
         <div className="p-2 flex-col justify-start items-start gap-0.5">
@@ -15,8 +8,8 @@ const FormPhone = () => {
                 <input 
                     id="phone" 
                     type="tel" 
-                    value={phone}
-                    onChange={handlePhoneChange}
+                    value={value}
+                    onChange={onChange}
                     className="w-full self-stretch h-12 p-3 bg-neutral-50 rounded justify-start items-start gap-2.5 flex-1" 
                     placeholder='Teléfono'
                     isRequired={false}
