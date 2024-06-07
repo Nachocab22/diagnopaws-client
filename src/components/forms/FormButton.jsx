e92dadd5af90a8693638294f67e3589b21c09fe9
Nachocab22 <ignacio.caballeromarin@alum.uca.es>
2024-06-07 13:06:46 +0200

    Update FormButton.jsx

--- src/components/forms/FormButton.jsx
+++ src/components/forms/FormButton.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-const FormButton = ({ text, color = "bg-slate-400 hover:bg-[#28464b] active:bg-slate-500", onClick = () => {} }) => {
+const FormButton = ({ text, color = "bg-slate-400 hover:bg-[#28464b] active:bg-slate-500"}) => {
     return (
         <div className="flex items-center justify-center">
             <button
                 type='submit'
                 className={`w-40 h-9 rounded-lg flex justify-center items-center ${color}`}
-                onClick={onClick}
             >
                 <span className="text-white text-base font-medium font-[IBM Plex Sans]">{text}</span>
             </button>
@@ -17,8 +16,7 @@ const FormButton = ({ text, color = "bg-slate-400 hover:bg-[#28464b] active:bg-s
 
 FormButton.propTypes = {
     text: PropTypes.string.isRequired,
-    color: PropTypes.string,
-    onClick: PropTypes.func,
+    color: PropTypes.string
 };
 
 export default FormButton;
