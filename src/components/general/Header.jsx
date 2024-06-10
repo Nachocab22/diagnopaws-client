0af51e30e39d83cb255f3e6f0755da7e5a99c709
Nachocab22 <ignacio.caballeromarin@alum.uca.es>
2024-06-10 08:27:48 +0200

    Update Header.jsx

--- src/components/general/Header.jsx
+++ src/components/general/Header.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
-const Header = ({handleActiveProfile, activeProfile}) => {
+const Header = ({ handleActiveProfile, activeProfile }) => {
     const navigate = useNavigate();
     const location = useLocation();
+    const [home, setHome] = useState('');
 
-    const isHomePage = location.pathname === '/pets';
+    useEffect(() => {
+        const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
+        if (user) {
+            user.role.includes('owner') ? setHome('/pets') : setHome('/manager');
+        }
+    }, []);
+
+    const isHomePage = location.pathname === home;
     const isMapPage = location.pathname === '/map';
 
     const homeButtonColor = isHomePage ? 'text-[#F39C12]' : 'text-black';
@@ -14,7 +22,7 @@ const Header = ({handleActiveProfile, activeProfile}) => {
     return (
         <header role="navigation" className="bg-[#fbfcfc] fixed h-24 w-full top-0 right-0 left-0 z-50 items-center">
             <button 
-                onClick={() => navigate('/pets')} 
+                onClick={() => navigate(home)}
                 className={`h-16 w-16 p-5 mx-5 ${homeButtonColor}`}
             >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-14 h-14">
@@ -23,7 +31,7 @@ const Header = ({handleActiveProfile, activeProfile}) => {
             </button>
             <button 
                 onClick={() => navigate('/map')} 
-                className="h-16 w-16 p-5 mx-5 active:text-[#F39C12]"
+                className={`h-16 w-16 p-5 mx-5 ${mapButtonColor}`}
             >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-14 h-14">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
