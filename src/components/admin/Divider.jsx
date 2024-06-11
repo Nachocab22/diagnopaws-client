e7ef1496d80c060afac9c93cc7a78a76fed33f4a
Nachocab22 <ignacio.caballeromarin@alum.uca.es>
2024-06-11 14:42:09 +0200

    Se han creado divisores para los componentes

--- src/components/admin/Divider.jsx
+++ src/components/admin/Divider.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Divider = (color) => {
     return (
-        <div className='w-auto h-0.5 border border-zinc-200 rounded-full md:hidden'></div>
+        <div className='w-auto h-0.5 border border-zinc-200 rounded-full my-3'></div>
     );
 };
 
---
+++ src/components/general/VerticalDivider.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const VerticalDivider = () => {
+    return (
+        <div className='w-0.5 h-auto border border-zinc-200 rounded-full hidden md:block'></div>
+    );
+};
+
+export default VerticalDivider;
